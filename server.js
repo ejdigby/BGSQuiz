7390220ddd6562794d1aff2b1f4489c804ef9491
Edward Digby <edwardjdigby@gmail.com>
2014-12-01 21:26:41 +0000

    Delete Scores On Program Start

--- server.js
+++ server.js
@@ -18,6 +18,20 @@ var url = 'mongodb://localhost:27017/bgsquiz';
 MongoClient.connect(url, function (err, db){
 if (err)throw err;
 console.log("Connected correctly to server");
+db.collection('Scores').update(
+    {score: { $gt : 0 }}, // query
+    {$set: {score: 0}}, // replacement, replaces only the field "hi"
+    {multi: true, upsert: false}, // options
+    function(err, object) {
+	if (err){
+	    console.warn(err.message);  // returns error if no matching object found
+	}else{
+	    console.dir(object);
+	}
+    }
+);
+
+
 
 var hbs = exphbs.create({
     // Specify helpers which are only registered on this instance.
