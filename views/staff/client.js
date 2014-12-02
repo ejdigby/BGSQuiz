138bcf2a8762238e6b5263f3fbb979cf62c86b48
James Brooks <james.brooks2@btinternet.com>
2014-12-02 09:43:34 +0000

    IT WORKS. IT IS ALIVE

--- views/staff/client.js
+++ views/staff/client.js
@@ -1,16 +1,16 @@
 $(document).ready(function() {
     var houseColors = ["#9C27B0", "#EF6C00", "#4CAF50", "#1565C0"]
     $('#house').change(function() {
-    if ($(this).val() == "behn") {
+    if ($(this).val() == "Behn") {
       $('body').css("background-color", houseColors[0]);
     }
-    else if ($(this).val() == "meitner") {
+    else if ($(this).val() == "Meitner") {
       $('body').css("background-color", houseColors[1]);
     }
-    else if ($(this).val() == "rorschach") {
+    else if ($(this).val() == "Rorschach") {
       $('body').css("background-color", houseColors[2]);
     }
-    else if ($(this).val() == "tinbergen") {
+    else if ($(this).val() == "Tinbergen") {
       $('body').css("background-color", houseColors[3]);
     }
     else {
@@ -21,7 +21,7 @@ $(document).ready(function() {
   $('#teamname').change(function() {
   if ($(this).val() == "add") {
      var newTeam = prompt('What is the name of the team that you would like to add?')
-     $('#teamname').append('<option selected>' + newTeam + '</option>').insertBefore($('#add'));
+     $('#teamname').append('<option selected>' + newTeam + '</option>').insertBefore();
   }
 }) 
 });
