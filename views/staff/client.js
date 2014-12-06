30d44f7e49609252d259591ee31d74c4b0dda6cb
James Brooks <james.brooks2@btinternet.com>
2014-12-06 20:29:18 +0000

    Remove handlebars.js

--- views/staff/client.js
+++ views/staff/client.js
@@ -23,5 +23,7 @@ $(document).ready(function() {
      var newTeam = prompt('What is the name of the team that you would like to add?')
      $('#teamname').append('<option selected>' + newTeam + '</option>').insertBefore();
   }
-}) 
+  })
+
+   
 });
